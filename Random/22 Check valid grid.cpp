f39f0a4e694c0ba3d6a07268ab3aeda369331a98
Ayush Sabale <ayushsabale2282@gmail.com>
2026-07-14 17:38:20 +0530

    Leetcode question 1391 submited

--- Random/22 Check valid grid.cpp
+++ Random/22 Check valid grid.cpp
@@ -7,113 +7,65 @@ using namespace std;
 class Solution
 {
 public:
-    pair<char, char> openings(int n)
+    bool hasValidPath(vector<vector<int>>& grid)
     {
-        pair<char, char> ans;
-        switch (n)
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // Directions: Left, Right, Up, Down
+        vector<vector<pair<int,int>>> dir = {
+            {},
+            {{0,-1},{0,1}},      // type 1
+            {{-1,0},{1,0}},      // type 2
+            {{0,-1},{1,0}},      // type 3
+            {{0,1},{1,0}},       // type 4
+            {{0,-1},{-1,0}},     // type 5
+            {{0,1},{-1,0}}       // type 6
+        };
+
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+
+        q.push({0,0});
+        vis[0][0] = 1;
+
+        while(!q.empty())
         {
-        case 1:
-            ans.first = 'L';
-            ans.second = 'R';
-            break;
-        case 2:
-            ans.first = 'U';
-            ans.second = 'B';
-            break;
-        case 3:
-            ans.first = 'L';
-            ans.second = 'B';
-            break;
-        case 4:
-            ans.first = 'R';
-            ans.second = 'B';
-            break;
-        case 5:
-            ans.first = 'L';
-            ans.second = 'U';
-            break;
-        case 6:
-            ans.first = 'R';
-            ans.second = 'U';
-            break;
+            auto [x,y] = q.front();
+            q.pop();
 
-        default:
-            ans.first = 'C';
-            ans.second = 'C';
-            break;
-        }
-
-        return ans;
-    }
-
-    bool isConnected(pair<int, int> &prev, pair<int, int> &curr)
-    {
-        
-        return true;
-    }
-
-    pair<int, int> next(pair<int, int> &prev, pair<int, int> &curr)
-    {
-        return curr;
-    }
-    pair<int, int> nextForHead(pair<int, int> &curr)
-    {
-        return curr;
-    }
-
-    bool hasValidPath(vector<vector<int>> &grid)
-    {
-        // m rows and n cols
-        int m = static_cast<int>(grid.size());
-        int n = static_cast<int>(grid[0].size());
-        if (n == 1 && m == 1)
-        {
-            return true;
-        }
-        if (grid[0][0] == 5)
-        {
-            return false;
-        }
-        pair<int, int> curr = {0, 0};
-        pair<int, int> prev = {0, 0};
-        if (grid[0][0] == 4)
-        {
-
-            // Speacial case
-        }
-
-        // initiate :
-        pair<int, int> newCurr = nextForHead(curr);
-        if (isConnected(curr, newCurr))
-        {
-            curr = newCurr;
-        }
-        else
-        {
-            return false;
-        }
-
-        // while loop
-        while (true)
-        {
-            if (curr.first == (m - 1) && curr.second == (n - 1))
-            {
+            if(x == m-1 && y == n-1)
                 return true;
-            }
 
-            newCurr = next(prev, curr);
-            if (isConnected(curr, newCurr))
+            for(auto [dx,dy] : dir[grid[x][y]])
             {
-                prev = curr;
-                curr = newCurr;
-            }
-            else
-            {
-                return false;
+                int nx = x + dx;
+                int ny = y + dy;
+
+                if(nx < 0 || nx >= m || ny < 0 || ny >= n)
+                    continue;
+
+                if(vis[nx][ny])
+                    continue;
+
+                // Check if neighbor connects back
+                for(auto [bdx,bdy] : dir[grid[nx][ny]])
+                {
+                    if(nx + bdx == x && ny + bdy == y)
+                    {
+                        vis[nx][ny] = 1;
+                        q.push({nx,ny});
+                        break;
+                    }
+                }
             }
         }
+
+        return false;
     }
 };
+
+
 
 int main()
 {
